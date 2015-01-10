7b76528a9c703f3824401e6667dbed6a6ad89984
Stu <maiflai@nospam>
2015-01-10 23:01:34 +0000

    zinc scalac does not support escaped parameters

--- src/test/groovy/org/scoverage/PluginAcceptanceTest.groovy
+++ src/test/groovy/org/scoverage/PluginAcceptanceTest.groovy
@@ -8,15 +8,26 @@ import static org.hamcrest.core.Is.is
 
 class PluginAcceptanceTest {
 
-    @Test
-    public void testProjectWithCompleteCoverage() throws Exception {
+    static def checkHappyDay(boolean useAnt) {
+        def projectRoot = "src/test/happy day"
         def build = GradleConnector.
-                            newConnector().
-                            forProjectDirectory(new File("src/test/happyday")).
-                            connect().newBuild()
+                newConnector().
+                forProjectDirectory(new File(projectRoot)).
+                connect().newBuild().
+                withArguments("-PuseAnt=$useAnt")
         build.forTasks('clean', 'checkScoverage').run()
 
-        def html = new File('src/test/happyday/build/reports/scoverage/index.html')
+        def html = new File("$projectRoot/build/reports/scoverage/index.html")
         assertThat('an HTML file should be created at ' + html.absolutePath, html.exists(), is(true))
+    }
+
+    @Test
+    public void testAntProjectWithCompleteCoverage() throws Exception {
+        checkHappyDay(true)
+    }
+
+    @Test
+    public void testZincProjectWithCompleteCoverage() throws Exception {
+        checkHappyDay(false)
     }
 }
