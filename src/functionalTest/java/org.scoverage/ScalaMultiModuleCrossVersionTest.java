269b17c71c3e4c935105b2606272bab878366673
Eyal Roth <eyalroth1@gmail.com>
2019-02-01 10:45:53 +0200

    Add a functional test for a multi-module project with multiple scala versions

---
+++ src/functionalTest/java/org.scoverage/ScalaMultiModuleCrossVersionTest.java
@@ -0,0 +1,59 @@
+package org.scoverage;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+
+public class ScalaMultiModuleCrossVersionTest extends ScoverageFunctionalTest {
+
+    public ScalaMultiModuleCrossVersionTest() {
+        super("scala-multi-module-cross-version");
+    }
+
+    @Test
+    public void checkAndAggregateScoverage() throws Exception {
+
+        AssertableBuildResult result = run("clean", ScoveragePlugin.getCHECK_NAME(),
+                ScoveragePlugin.getAGGREGATE_NAME());
+
+        result.assertTaskSkipped(ScoveragePlugin.getREPORT_NAME());
+        result.assertTaskSucceeded("2_11:" + ScoveragePlugin.getREPORT_NAME());
+        result.assertTaskSucceeded("2_12:" + ScoveragePlugin.getREPORT_NAME());
+        result.assertTaskSucceeded(ScoveragePlugin.getCHECK_NAME());
+        result.assertTaskSucceeded("2_11:" + ScoveragePlugin.getCHECK_NAME());
+        result.assertTaskSucceeded("2_12:" + ScoveragePlugin.getCHECK_NAME());
+        result.assertTaskSucceeded(ScoveragePlugin.getAGGREGATE_NAME());
+
+        assertAllReportFilesExist();
+        assertCoverage(100.0);
+    }
+
+    private void assertAllReportFilesExist() {
+
+        assert211ReportFilesExist();
+        assert212ReportFilesExist();
+        assertAggregationFilesExist();
+    }
+
+    private void assertAggregationFilesExist() {
+
+        Assert.assertTrue(resolve(reportDir(), "index.html").exists());
+        Assert.assertTrue(resolve(reportDir(), "2_11/src/main/scala/org/hello/World211.scala.html").exists());
+        Assert.assertTrue(resolve(reportDir(), "2_12/src/main/scala/org/hello/World212.scala.html").exists());
+    }
+
+    private void assert211ReportFilesExist() {
+
+        File reportDir = reportDir(projectDir().toPath().resolve("2_11").toFile());
+        Assert.assertTrue(resolve(reportDir, "index.html").exists());
+        Assert.assertTrue(resolve(reportDir, "src/main/scala/org/hello/World211.scala.html").exists());
+    }
+
+    private void assert212ReportFilesExist() {
+
+        File reportDir = reportDir(projectDir().toPath().resolve("2_12").toFile());
+        Assert.assertTrue(resolve(reportDir, "index.html").exists());
+        Assert.assertTrue(resolve(reportDir, "src/main/scala/org/hello/World212.scala.html").exists());
+    }
+}
---
+++ src/functionalTest/resources/projects/scala-multi-module-cross-version/2_12/src/main/scala/org/hello/World212.scala
@@ -0,0 +1,9 @@
+package org.hello
+
+class World212 {
+
+  def foo(): String = {
+    val s = "2" + "12"
+    s
+  }
+}
