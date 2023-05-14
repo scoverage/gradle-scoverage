1641c841ab41c7e376a4c37d8e20aa675e42577b
Stu <maiflai@users.noreply.github.com>
2023-05-14 22:22:49 +0100

    fix scala 3.2 test

--- src/crossScalaVersionTest/resources/projects/scala-multi-module-cross-version/3_2/src/test/scala/org/hello/World3_2Suite.scala
+++ src/crossScalaVersionTest/resources/projects/scala-multi-module-cross-version/3_2/src/test/scala/org/hello/World3_2Suite.scala
@@ -1,11 +1,11 @@
 package org.hello
 
 import org.junit.runner.RunWith
-import org.scalatest.FunSuite
-import org.scalatest.junit.JUnitRunner
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatestplus.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class World3_2Suite extends FunSuite {
+class World3_2Suite extends AnyFunSuite {
 
   test("foo") {
     new World3_2().foo()
